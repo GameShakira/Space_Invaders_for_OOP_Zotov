13b8963e5f002f5cdd084e3be6d8b1100c144bf9
Game_Shakira <103584080+GameShakira@users.noreply.github.com>
2023-10-18 23:01:30 +0300

    50/50

--- main.py
+++ main.py
@@ -10,33 +10,43 @@ from hero import Character
 #███████╗╚█████╔╝░░╚██╔╝░░███████╗
 #╚══════╝░╚════╝░░░░╚═╝░░░╚══════╝
 
-
+RIGHT = "to the right"
+LEFT = "to the left"
+STOP = "stop"
+motion = STOP
 
 def start_game():
     pygame.init()
     screen = pygame.display.set_mode((600, 700))
     pygame.display.set_caption("Space Y")
-    character = Character(screen, 100, -410)
+    character = Character(screen, 0, 0)
+    x = 0
 
     flag = True 
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-                left = True
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-                right = True
-            if event.type == pygame.KEYUP and event.key == pygame.K_d:
-                right = False
-            if event.type == pygame.KEYUP and event.key == pygame.K_a:
-                left = False
-               
+            elif event.type == pygame.KEYDOWN:
+             if event.key == pygame.K_LEFT:
+                motion = LEFT
+             if event.key == pygame.K_RIGHT:
+                motion = RIGHT
+            elif event.type == pygame.KEYUP:
+             if event.key in [pygame.K_LEFT,
+                         pygame.K_RIGHT]:
+                motion = STOP
+                
+
+             if motion == LEFT:
+              x -= 3
+              print("a")
+             elif motion == RIGHT:
+              x += 3
 
         character.output()
         pygame.display.flip()
         
-
-
+pygame.display.update()
 
 start_game()
