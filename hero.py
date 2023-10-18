fa8fa5ad53d4bbdb43d39ca373498bebb8e29d53
Game_Shakira <103584080+GameShakira@users.noreply.github.com>
2023-10-18 21:52:09 +0300

    add not way

--- hero.py
+++ hero.py
@@ -1,5 +1,33 @@
 import pygame
 
+MOVE_SPEED = 3
+
+
 class Character():
-   def __init__(self, image):
-      self.image =  pygame.image.load(images/hero1.png)
+   def __init__(self, screen, x, y):
+      self.xvel = 0  
+      self.startX = x
+      self.screen = screen
+      self.image =  pygame.image.load("images/hero1.png")
+      self.rect = self.image.get_rect()
+      self.screen_rect = screen.get_rect()
+      self.rect.centerx = self.screen_rect.centerx
+      self.rect.bottom = self.screen_rect.bottom
+
+   def update(self,  left, right):
+        if left:
+            self.xvel = -MOVE_SPEED 
+ 
+        if right:
+            self.xvel = MOVE_SPEED 
+         
+        if not(left or right): 
+            self.xvel = 0
+
+        self.rect.x += self.xvel
+
+
+
+
+   def output(self):
+      self.screen.blit(self.image, (self.rect.x,self.rect.y))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+from hero import Character
 
 
 #██╗░░░░░░█████╗░██╗░░░██╗███████╗
@@ -14,15 +14,29 @@ import sys
 
 def start_game():
     pygame.init()
-    screen = pygame.display.set_mode((1600, 900))
+    screen = pygame.display.set_mode((600, 700))
     pygame.display.set_caption("Space Y")
+    character = Character(screen, 100, -410)
 
     flag = True 
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+                left = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                right = True
+            if event.type == pygame.KEYUP and event.key == pygame.K_d:
+                right = False
+            if event.type == pygame.KEYUP and event.key == pygame.K_a:
+                left = False
+               
 
+        character.output()
         pygame.display.flip()
+        
+
+
 
 start_game()
