fda7edfd060a91d5e62d67ced99740b62b876c88
Andy <andreialsh312003@gmail.com>
2023-10-27 13:58:18 +0300

    add

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-import pygame
-import sys
-from Hero import MainCharacter
+import pygame, controls
+from main_character import MainCharacter
+from pygame.sprite import Group
 
-def start_game ():
+def start_game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 800))
-    pygame.display.set_caption("SpaceX by Andrei")
+    pygame.display.set_caption("SpaxeX by Pushkova")
+
+    maincharacter = MainCharacter(screen)
+    bullets = Group()
 
     flag = True
-    hero = MainCharacter(screen)
     while flag:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    hero.move_right = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    hero.move_right = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    hero.move_left = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    hero.move_left = False
-
-        hero.output()
+        controls.events(screen, maincharacter, bullets)
+        maincharacter.output()
         pygame.display.flip()
-        hero.moving(screen)
+        maincharacter.moving(screen)
+
+        controls.update(screen, maincharacter, bullets)
+        controls.update_bullets(screen,bullets)
 
 start_game()
